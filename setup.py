836a10e2deb5edabcac1de74299384f5d952a5eb
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2020-07-01 07:10:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
 )
