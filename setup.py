a588a66d9038ea5bfa424dc865f776ac26118449
LeoneBacciu <leonebacciu@gmail.com>
2020-07-20 14:31:43 +0200

    New version and automatic deploy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-email-verification",
-    version="0.0.5.post2",
+    version="0.0.6",
     author="Leone Bacciu",
     author_email="leonebacciu@gmail.com",
     description="Email confirmation app for django",
