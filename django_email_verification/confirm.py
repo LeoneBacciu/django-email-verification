f67ef0dc693e01a85c4105863e8a80aec8553697
LeoneBacciu <leonebacciu@gmail.com>
2021-01-13 18:31:26 +0100

    Fix missing column and import

--- django_email_verification/confirm.py
+++ django_email_verification/confirm.py
@@ -1,15 +1,17 @@
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 from binascii import Error as b64Error
-from django.conf import settings
-from django.contrib.auth import get_user_model, login, logout
-from django.contrib.auth.tokens import default_token_generator
-from django.template.loader import render_to_string
-from django.urls import get_resolver
-from django.utils import timezone
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP
 from threading import Thread
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.urls import get_resolver
+from django.utils import timezone
 
 from .errors import InvalidUserModel, EmailTemplateNotFound, NotAllFieldCompiled
 
@@ -20,10 +22,8 @@ def sendConfirm(user, **kwargs):
         setattr(user, active_field, False)
         user.save()
 
-        try:
+        if kwargs.get('custom_salt'):
             default_token_generator.key_salt = kwargs['custom_salt']
-        except:
-            pass
 
         try:
             token = kwargs['token']
@@ -49,7 +49,8 @@ def sendConfirm_thread(email, token):
     mail_html = validateAndGetField('EMAIL_MAIL_HTML', raise_error=False)
 
     if not (mail_plain or mail_html):  # Validation for mail_plain and mail_html as both of them have raise_error=False
-        raise NotAllFieldCompiled(f"Both EMAIL_MAIL_PLAIN and EMAIL_MAIL_HTML missing from settings.py, at least one of them is required.")
+        raise NotAllFieldCompiled(
+            f"Both EMAIL_MAIL_PLAIN and EMAIL_MAIL_HTML missing from settings.py, at least one of them is required.")
 
     domain += '/' if not domain.endswith('/') else ''
 
@@ -84,9 +85,9 @@ def sendConfirm_thread(email, token):
     if not msg.get_payload():
         raise EmailTemplateNotFound('No email template found')
 
-    email_backend  = validateAndGetField('EMAIL_BACKEND')
+    email_backend = validateAndGetField('EMAIL_BACKEND')
 
-    if email_backend and email_backend == 'django.core.mail.backends.console.EmailBackend'
+    if email_backend and email_backend == 'django.core.mail.backends.console.EmailBackend':
         msg = EmailMessage(subject, msg.as_string(), sender, [email])
         if mail_html:
             msg.content_subtype = "html"
