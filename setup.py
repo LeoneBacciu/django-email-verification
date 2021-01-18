65a61c55bb71d9b7a6919ee14a2dd7bde6721a31
LeoneBacciu <leonebacciu@gmail.com>
2021-01-18 12:18:17 +0100

    Update version for publishing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-email-verification",
-    version="0.0.8",
+    version="0.0.9",
     author="Leone Bacciu",
     author_email="leonebacciu@gmail.com",
     description="Email confirmation app for django",
