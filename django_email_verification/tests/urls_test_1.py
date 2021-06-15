dcfb970ca0ae39ab738a1b6c5b7ecb7c86c4865c
LeoneBacciu <leonebacciu@gmail.com>
2021-06-15 23:03:41 +0200

    Add test for named views

--- django_email_verification/tests/urls_test_1.py
+++ django_email_verification/tests/urls_test_1.py
@@ -5,4 +5,5 @@ from django_email_verification import urls, verify_view
 urlpatterns = [
     path('email/', include(urls)),
     path('confirm/<str:token>/', verify_view(lambda request, token: None)),
+    path('named_view/', lambda request: None, name='named_view_name'),
 ]
